30d8ecb5c715833b32cb8d2457d5f14e29430830
SEBASTIAN BRAND TAMAYO <sebastianbrand232390@correo.itm.edu.co>
2023-07-24 20:21:13 -0500

    Add States Numbers

--- Sales.API/Program.cs
+++ Sales.API/Program.cs
@@ -1,18 +1,19 @@
 using Microsoft.EntityFrameworkCore;
 using Sales.API.Data;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=DefaultConnection"));
 builder.Services.AddTransient<SeedDb>();
 
-var app = builder.Build();
+var app = builder.Build(); 
 SeedData(app);
 
 void SeedData(WebApplication app)
---
+++ Sales.Shared/Entities/State.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Sales.Shared.Entities
+{
+    public class State
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener más de {1} caractéres")]
+        [Display(Name = "Estado/Departamento")]
+        public string Name { get; set; } = null!;
+        public int CountryId { get; set; }
+
+        public Country? Country { get; set; }
+
+        public ICollection<City>? Cities { get; set; }
+
+        public int CitiesNumber => Cities == null ? 0 : Cities.Count;
+    }
+}
