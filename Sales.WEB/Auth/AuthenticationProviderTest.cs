97e033cab1f8043e3a1f89195f3bc291f6b5c932
Sebastian <Sebastianbrand046@outlook.com>
2023-08-03 14:07:54 -0500

    Security hardcoded on Web

---
+++ Sales.WEB/Auth/AuthenticationProviderTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Sales.WEB.Auth
+{
+    public class AuthenticationProviderTest : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var anonimous = new ClaimsIdentity();
+            var zuluUser = new ClaimsIdentity(new List<Claim>
+        {
+            new Claim("FirstName", "Sebastián"),
+            new Claim("LastName", "Brand"),
+            new Claim(ClaimTypes.Name, "brand@yopmail.com"),
+            new Claim(ClaimTypes.Role, "Admin")
+        },
+
+                authenticationType: "test");
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(anonimous)));
+
+        }
+    }
+}
